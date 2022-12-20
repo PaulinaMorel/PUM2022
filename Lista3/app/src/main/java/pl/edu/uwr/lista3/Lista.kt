7b1e301b6dbf63a44dac42a0a09b427b3f93b8cd
PaulinaMorel <92079111+PaulinaMorel@users.noreply.github.com>
2022-12-20 21:19:01 +0100

    $git commit -m "Lista3"

--- Lista3/app/src/main/java/pl/edu/uwr/lista3/Lista.kt
+++
@@ -1,9 +0,0 @@
-package pl.edu.uwr.lista3
-
-data class Lista(val name: String, val index: Int) {
-    var id: Int = 0
-
-    constructor(id: Int, name: String, index: Int) : this(name, index) {
-        this.id = id
-    }
-}
